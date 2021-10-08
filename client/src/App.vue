a5e8b37df10404de60d9d770054eae8c8a410361
qhhivan <orsolic.i02@htlwienwest.at>
2021-10-08 14:48:49 +0200

    Daten vom Server geholt

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,8 +9,29 @@
 </template>
 
 <script>
+// IMPORT
+import axios from 'axios';
+
 export default {
   name: 'App',
-  data: () => ({}),
+  data: () => ({
+    allCars: [],
+  }),
+
+  methods: {
+    async getCars() {
+      try {
+        const { data } = await axios.get('http://localhost:3000/cars');
+        this.allCars = data;
+        console.log(this.allCars);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+
+  created () {
+    this.getCars();
+  },
 };
 </script>
