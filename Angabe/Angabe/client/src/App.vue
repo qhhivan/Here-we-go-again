598a7759ac7e32e2a3cabf201b2256ff9d33abcf
qhhivan <orsolic.i02@htlwienwest.at>
2021-10-08 14:24:03 +0200

    Start

---
+++ Angabe/Angabe/client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <p class="text-h2 text-center">Viel Erfolg!</p>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({}),
+};
+</script>
